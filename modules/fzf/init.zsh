f55f734fbcd46f30279be9776ec14966672fcb23
Bernhard Waldbrunner <bernhard.waldbrunner@netural.com>
2016-06-01 16:18:03 +0200

    Removed PATH and MANPATH modification from fzf module

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -1,15 +1,3 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
@@ -17,4 +5,3 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-
